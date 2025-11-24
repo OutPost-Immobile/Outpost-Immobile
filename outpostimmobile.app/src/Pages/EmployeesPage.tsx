d57c42480b4763970a0f88247bbb92ebb7658105
Michał Rodak <xoriner@gmail.com>
2025-11-24 21:49:24 +0100

    add a basic test for EmployeesPage.tsx

---
+++ outpostimmobile.app/src/Pages/EmployeesPage.tsx
@@ -0,0 +1,11 @@
+import {Typography} from "@mui/material";
+
+export const EmployeesPage = () => {
+    return (
+        <>
+            <div>
+                <Typography variant="h4">AAAAAAAAAAAAAAAaa</Typography>
+            </div>
+        </>
+    )
+}
