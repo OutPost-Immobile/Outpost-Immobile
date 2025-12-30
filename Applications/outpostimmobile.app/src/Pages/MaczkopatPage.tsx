01e653183d7910ef6e00404082f42986bc3a780b
Phario <grzedyn@gmail.com>
2025-12-30 22:16:41 +0100

    Add maczkopatPage page and extend parcel seeder

--- Applications/outpostimmobile.app/src/Pages/MaczkopatPage.tsx
+++ Applications/outpostimmobile.app/src/Pages/MaczkopatPage.tsx
@@ -1,16 +1,72 @@
-﻿import {Button, Stack, Paper} from "@mui/material";
+﻿import { Button, Stack, Paper, TextField } from "@mui/material";
+import { DataGrid, type GridColDef } from "@mui/x-data-grid";
+import type { ParcelDto, TypedResponse } from "../Models/Types.ts";
+import { useState } from "react";
 
 export const MaczkopatPage = () => {
-    // TODO: finish this page
+
+    const [maczkopatId, setMaczkopatId] = useState("");
+    const [parcels, setParcels] = useState<ParcelDto[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    const handleButtonClick = async () => {
+        setLoading(true);
+        setError("");
+
+        try {
+            const response = await fetch(`http://localhost:5188/api/Parcels/Maczkopat/${maczkopatId}`);
+            const result: TypedResponse<ParcelDto[]> = await response.json();
+
+            if (result.data) {
+                setParcels(result.data);
+            } else if (result.errors) {
+                console.error(result.errors);
+            }
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const columns: GridColDef[] = [
+        {field: "friendlyId", headerName: "Friendly ID", width: 300},
+        {field: "status", headerName: "Parcel status", width: 300}
+    ];
+
     return (
         <Stack spacing={2} style={{justifyContent: 'center', alignItems: 'center', paddingTop: 64}}>
-            <Paper elevation={6} style={{padding: 16, margin: 64}}>
-
+            <Paper elevation={6} style={{padding: 16, margin: 16}}>
+                <Stack spacing={2} style={{justifyContent: 'center', alignItems: 'center', paddingTop: 16}}>
+                    <TextField
+                        required
+                        id="id-field"
+                        label="ID Maczkopatu"
+                        variant="outlined"
+                        value={maczkopatId}
+                        onChange={(e) => setMaczkopatId(e.target.value)}
+                        error={!!error}
+                        sx={{width: 500}}
+                    />
+                    <Button
+                        variant="outlined"
+                        onClick={handleButtonClick}>
+                    </Button>
+                </Stack>
             </Paper>
-            <Paper elevation={6} style={{padding: 16, margin: 64}}>
-                <Button variant="outlined">
-                    papieżak polak 2137
-                </Button>
+            <Paper elevation={6} style={{padding: 16}}>
+                <DataGrid
+                    rows={parcels.map((p, index) => ({
+                        id: index,
+                        friendlyId: p.friendlyId,
+                        status: p.status
+                    }))}
+                    columns={columns}
+                    autoHeight
+                    loading={loading}
+                    pageSizeOptions={[5, 10, 25]}
+                ></DataGrid>
             </Paper>
         </Stack>
     )
