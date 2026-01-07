58116020a6adf99bbabb28a0c2ae374e7f1141c4
Phario <grzedyn@gmail.com>
2026-01-07 02:09:28 +0100

    In the name of god I FIX THIS PAGE

--- Applications/OutpostImmobile.App/src/Pages/LandingPage.tsx
+++ Applications/OutpostImmobile.App/src/Pages/LandingPage.tsx
@@ -1,7 +1,25 @@
-import {Grid2, Paper, Typography} from "@mui/material";
+import {Button, Grid2, Paper, Stack, Typography} from "@mui/material";
 import LOGO  from "../assets/image2vector.svg";
+import {useAuth} from "../Auth/AuthProvider.tsx";
+import {useState} from "react";
+import {useNavigate} from "react-router"; // Add this import
 
 export const LandingPage = () => {
+    const { isAuthenticated } = useAuth();
+    const [loading] = useState(false);
+    const navigate = useNavigate(); // Add this hook
+
+    const handleMaczkopatState = () => {
+        navigate('/Maczkopat');
+    }
+
+    const handleRouteListPath = () => {
+        navigate('/Routes');
+    }
+
+    const handleParcelStateUpdatePath = () => {
+        navigate('/Infrastructure'); //TODO: Placeholder zanim napiszę stronę od zmiany stanu paczki
+    }
 
     return (
         <>
@@ -16,30 +34,64 @@ export const LandingPage = () => {
                         Zaufaj nam, a my zrobimy resztę
                     </Typography>
                 </Grid2>
-                <Grid2 size={4}>
-                    <Paper elevation={3} style={{padding: 16, marginInline: 16}}>
-                        <Typography variant="h5" style={{fontWeight: 'bold', textAlign: 'center'}}>
-                            Nadaj Paczkę
-                        </Typography>
-                        <img src={LOGO} alt="Outpost" style={{width: 256, height: 256, display: 'block', margin: 'auto'}}/>
-                    </Paper>
-                </Grid2>
-                <Grid2 size={4}>
-                    <Paper elevation={3} style={{padding: 16, marginInline: 16}}>
-                        <Typography variant="h5" style={{fontWeight: 'bold', textAlign: 'center'}}>
-                            Zaskocz nas
-                        </Typography>
-                        <img src={LOGO} alt="Outpost" style={{width: 256, height: 256, display: 'block', margin: 'auto'}}/>
-                    </Paper>
-                </Grid2>
-                <Grid2 size={4}>
-                    <Paper elevation={3} style={{padding: 16, marginInline: 16}}>
-                        <Typography variant="h5" style={{fontWeight: 'bold', textAlign: 'center'}}>
-                            Śledź Paczkę
-                        </Typography>
-                        <img src={LOGO} alt="Outpost" style={{width: 256, height: 256, display: 'block', margin: 'auto'}}/>
-                    </Paper>
-                </Grid2>
+                
+                {!isAuthenticated ? (
+                    <>
+                        <Grid2 size={4}>
+                            <Paper elevation={3} style={{padding: 16, marginInline: 16}}>
+                                <Typography variant="h5" style={{fontWeight: 'bold', textAlign: 'center'}}>
+                                    Nadaj Paczkę
+                                </Typography>
+                                <img src={LOGO} alt="Outpost" style={{width: 256, height: 256, display: 'block', margin: 'auto'}}/>
+                            </Paper>
+                        </Grid2>
+                        <Grid2 size={4}>
+                            <Paper elevation={3} style={{padding: 16, marginInline: 16}}>
+                                <Typography variant="h5" style={{fontWeight: 'bold', textAlign: 'center'}}>
+                                    Zaskocz nas
+                                </Typography>
+                                <img src={LOGO} alt="Outpost" style={{width: 256, height: 256, display: 'block', margin: 'auto'}}/>
+                            </Paper>
+                        </Grid2>
+                        <Grid2 size={4}>
+                            <Paper elevation={3} style={{padding: 16, marginInline: 16}}>
+                                <Typography variant="h5" style={{fontWeight: 'bold', textAlign: 'center'}}>
+                                    Śledź Paczkę
+                                </Typography>
+                                <img src={LOGO} alt="Outpost" style={{width: 256, height: 256, display: 'block', margin: 'auto'}}/>
+                            </Paper>
+                        </Grid2>
+                    </>
+                ) : (
+                    <Stack spacing={2} style={{justifyContent: 'center', alignItems: 'center', paddingTop: 64}}>
+                        <Paper elevation={6} style={{padding: 16}}>
+                            <Stack spacing={5}>
+                                <Button
+                                    onClick={handleMaczkopatState}
+                                    disabled={loading}
+                                    variant="contained"
+                                    sx={{width: 500, height: 64, color: '#323232', backgroundColor: '#FFDE59'}}>
+                                    Stan maczkopatu
+                                </Button>
+                                <Button
+                                    onClick={handleRouteListPath}
+                                    disabled={loading}
+                                    variant="contained"
+                                    sx={{width: 500, height: 64, color: '#323232', backgroundColor: '#FFDE59'}}>
+                                    Lista tras
+                                </Button>
+                                <Button
+                                    onClick={handleParcelStateUpdatePath}
+                                    disabled={loading}
+                                    variant="contained"
+                                    sx={{width: 500, height: 64, color: '#323232', backgroundColor: '#FFDE59'}}>
+                                    Aktualizacja stanu paczki
+                                </Button>
+                            </Stack>
+                        </Paper>
+                    </Stack>
+                ) }
+
                 <Grid2 size={12}>
                     <Typography variant="h5" style={{fontWeight: 'bold', textAlign: 'center', paddingTop: 16}}>
                         Zaufało nam już dużo ludzi!
---
+++ Applications/OutpostImmobile.App/src/Pages/TrackingPage.tsx
@@ -0,0 +1,91 @@
+﻿import {Button, Stack, Paper, TextField, Typography} from "@mui/material";
+import { DataGrid, type GridColDef } from "@mui/x-data-grid";
+import { useState } from "react";
+import {$api} from "../Api/Api.ts";
+import {GET_METHOD, PARCEL_LOGS_URL} from "../Consts.ts";
+
+export const TrackingPage = () => {
+    const [parcelId, setParcelId] = useState("");
+    const [searchId, setSearchId] = useState<string | null>(null);
+
+    const { data, isLoading, isError } = $api.useQuery(
+        GET_METHOD,
+        PARCEL_LOGS_URL,
+        {
+            params: {
+                path: { parcelId: searchId as string },
+            },
+        },
+        {
+            enabled: !!searchId,
+        }
+    );
+
+    const handleButtonClick = () => {
+        if (parcelId.trim()) {
+            setSearchId(parcelId);
+        }
+    }
+
+    const parcels = data?.data ?? [];
+
+    const columns: GridColDef[] = [
+        { field: "id", headerName: "#", width: 70 },
+        { field: "friendlyId", headerName: "ID", width: 215 },
+        { field: "status", headerName: "Status", width: 215 }
+    ];
+
+    return (
+        <Stack spacing={2} style={{justifyContent: 'center', alignItems: 'center', paddingTop: 16}}>
+            <Paper elevation={6} style={{padding: 16, margin: 16}}>
+                <Stack spacing={5} style={{justifyContent: 'center', alignItems: 'center', paddingTop: 16}}>
+                    <Typography variant="h5" style={{fontWeight: 'bold', textAlign: 'center'}}>
+                        Śledzenie paczki
+                    </Typography>
+                    <TextField
+                        required
+                        id="id-field"
+                        label="ID Maczkopatu"
+                        variant="outlined"
+                        value={parcelId}
+                        onChange={(e) => setParcelId(e.target.value)}
+                        error={isError}
+                        sx={{width: 500}}
+                    />
+                    <Button
+                        onClick={handleButtonClick}
+                        variant="contained"
+                        sx={{width: 500, height: 64, color: '#323232', backgroundColor: '#FFDE59'}}>
+                        Sprawdź
+                    </Button>
+                </Stack>
+            </Paper>
+            <Paper elevation={6} style={{padding: 16}}>
+                <DataGrid
+                    rows={parcels.map((p, index) => ({
+                        id: index + 1,
+                        friendlyId: p.friendlyId,
+                        status: p.status
+                    }))}
+                    columns={columns}
+                    loading={isLoading}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { pageSize: 5 },
+                        },
+                    }}
+                    pageSizeOptions={[5]}
+                    sx={{
+                        boxShadow: 2,
+                        border: 2,
+                        borderColor: '#FFDE59',
+                        "& .MuiDataGrid-footerContainer": {
+                            backgroundColor: "#FFDE59",
+                            color: "black",
+                        }
+                    }}
+                />
+            </Paper>
+        </Stack>
+    )
+}
