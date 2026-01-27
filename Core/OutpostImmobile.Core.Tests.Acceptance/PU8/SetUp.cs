9a2f2fd1075eec46ab59f931ae936935a5b6faef
Phario <155861557+Phario@users.noreply.github.com>
2026-01-27 22:05:23 +0100

    [IO] Add UpdateParcelStatusTest (#82)

---
+++ Core/OutpostImmobile.Core.Tests.Acceptance/PU8/SetUp.cs
@@ -0,0 +1,17 @@
+﻿using fit;
+
+namespace OutpostImmobile.Core.Tests.Acceptance.PU8;
+
+public class SetUp : Fixture
+{
+    // statyczne instancje “realizatora przypadku użycia” + “stan”
+    public static UpdateParcelStatusUseCase UpdateParcelStatus = null!;
+    public static ParcelStatusState ParcelState = null!;
+
+    public SetUp()
+    {
+        // tu składasz i łączysz “części systemu”
+        ParcelState = new ParcelStatusState();
+        UpdateParcelStatus = new UpdateParcelStatusUseCase(ParcelState);
+    }
+}
